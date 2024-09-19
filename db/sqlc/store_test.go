55e37e1206e9feda0f4b1fe000f4427cbbf0cf11
Lei Chen <mashirocl@gmail.com>
2024-09-19 16:25:03 +0900

    Initial commit

---
+++ db/sqlc/store_test.go
@@ -0,0 +1,52 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestTransferTx(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := CreateRandomAccount(t)
+	account2 := CreateRandomAccount(t)
+
+	n := 5
+	amount := int64(10)
+
+	errs := make(chan error)
+	results := make(chan TransferTxResult)
+
+	for i := 0; i < n; i++ {
+		go func() {
+			result, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountID: account1.ID,
+				ToAccountID:   account2.ID,
+				Amount:        amount,
+			})
+			errs <- err
+			results <- result
+		}()
+	}
+
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+		result := <-results
+		require.NotEmpty(t, result)
+
+		transfer := result.Transfer
+		_, err = store.GetTransfer(context.Background(), transfer.ID)
+		require.NoError(t, err)
+
+		fromEntry := result.FromEntry
+		_, err = store.GetEntry(context.Background(), fromEntry.ID)
+		require.NoError(t, err)
+
+		toEntry := result.ToEntry
+		_, err = store.GetEntry(context.Background(), toEntry.ID)
+		require.NoError(t, err)
+	}
+}
---
+++ util/random.go
@@ -0,0 +1,39 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+	for i := 0; i < n; i++ {
+		sb.WriteByte(alphabet[rand.Intn(k)])
+	}
+	return sb.String()
+}
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomMoney() int64 {
+	return RandomInt(0, 999999)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"JPY", "CNY", "USD"}
+	return currencies[rand.Intn(len(currencies))]
+}
